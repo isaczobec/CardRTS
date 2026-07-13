f5651dadbb77888ceddead6966c1412fb196015b
Isac Zobec <zobec.isac@gmail.com>
2026-07-14 01:15:14 +0200

    increase world size

--- Assets/Scripts/WorldGen/WorldGen.cs
+++ Assets/Scripts/WorldGen/WorldGen.cs
@@ -12,7 +12,7 @@ public class WorldGenHandler
     /// <summary>
     /// The suqare root of the amount of chunks in the world. 
     /// </summary>
-    public static ushort WorldSizeChunks = 16;
+    public static ushort WorldSizeChunks = 24;
     private TileType[] _tiles;
     private float[] _heightMap;
 
